7ec145950b72282da95eac1f2ad4c8bc13ea4ed1
VictorKrylach23 <vkrya2023@gmail.com>
2025-01-19 12:15:04 -0500

    Init new project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+
+const Div = styled.div`
+	text-align: center;
+`;
+
+export const App = () => {
+	return (
+		<Div>
+			<i className="fa fa-battery-three-quarters" aria-hidden="true"></i>
+			<br></br>
+			333333
+		</Div>
+	);
+};
